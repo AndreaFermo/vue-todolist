74f0594559f524b0cf374af27d8bbc09ffd94cf2
Andrea Fermo <andreafermo.sd@gmail.com>
2023-03-16 17:36:12 +0100

    milestone 3 concluso

--- js/main.js
+++ js/main.js
@@ -4,24 +4,39 @@ createApp({
   data() {
     return {
       list: [
-        {
-            text: 'prova1',
+            {
+                text: 'Farmare materiali',
+                done: false
+            },
+            {
+                text: 'Ottenere arma',
+                done: true
+            },
+            {
+                text: 'Livellare equipaggiamento',
+                done: false
+            },
+        ],
+        newElement: {
+            text: '',
             done: false
         },
-        {
-            text: 'prova2',
-            done: true
-        },
-        {
-            text: 'prova3',
-            done: false
-        },
-      ],
+        error: false  
     }
   },
   methods: {
     removeElement(index) {
         this.list.splice(index, 1);
     },
+    addElement() {
+        if (this.newElement.text.length >= 5) {
+        const newTask = {...this.newElement} ;
+        this.list.unshift(newTask);
+        this.newElement.text = '';
+        this.error = false;
+        } else {
+            this.error = true;
+        }
+    }
   }
 }).mount('#app')
